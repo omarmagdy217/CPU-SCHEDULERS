aa1c34a7c29556263d03d284346628083198dc8d
omarmagdy217 <36456356+omarmagdy217@users.noreply.github.com>
2019-04-09 02:11:44 +0200

    Non Preemptive Priority Enhance

--- Main Code/PRIORITY.cs
+++ Main Code/PRIORITY.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,56 +8,80 @@ namespace CPU_SCHEDULERS
 {
     class PRIORITY
     {
+        struct Process
+        {
+            public int pid;  // Process ID 
+            public int at;   // arrival time
+            public int bt;   // CPU Burst time required 
+            public int priority; // Priority of this process 
+        };
+
         public static void prioritySort(int size, int[] at, int[] bt, int[] prio, int[] st, int[] ps)
         {
-            int temp,t_sum=0;
+            int t_sum = at[0], min_at, min_index, i, j = 0;
+            IList<Process> pr = new List<Process>();
+            Process temp;
+            for (i = 0; i < size; i++)
+            {
+                temp.pid = ps[i];
+                temp.at = at[i];
+                temp.bt = bt[i];
+                temp.priority = prio[i];
+                pr.Add(temp);
+            }
             for (int iter = 0; iter < (size - 1); iter++)
             {
-                for (int i = 0; i < (size - 1-iter); i++)
+                for (i = 0; i < (size - 1-iter); i++)
                 {
-                    if (at[i] == at[i + 1])
+                    if (pr[i].priority > pr[i+1].priority)
                     {
-                        if (prio[i] > prio[i + 1])
-                        {
-                            temp = ps[i];
-                            ps[i] = ps[i + 1];
-                            ps[i + 1] = temp;
-                            temp = at[i];
-                            at[i] = at[i + 1];
-                            at[i + 1] = temp;
-                            temp = bt[i];
-                            bt[i] = bt[i + 1];
-                            bt[i + 1] = temp;
-                            temp = prio[i];
-                            prio[i] = prio[i + 1];
-                            prio[i + 1] = temp;
-                        }
+                        temp = pr[i];
+                        pr[i] = pr[i+1];
+                        pr[i+1] = temp;
                     }
                 }
             }
-            for (int i = 0; i <= size; i++)
+            while (pr.Count > 0)
             {
-                if (i == 0)
+                min_at = pr[0].at;
+                min_index = 0;
+                for (i = 0; i < pr.Count; i++)
                 {
-                    t_sum += at[i];
-                    st[i] = t_sum;
+                    if (pr[i].at <= t_sum)
+                        break;
+                    else if (pr[i].at < min_at)
+                    {
+                        min_at = pr[i].at;
+                        min_index = i;
+                    }
+                }
+                if (i == pr.Count)
+                {
+                    st[j] = pr[min_index].at;
+                    ps[j] = pr[min_index].pid;
+                    at[j] = pr[min_index].at;
+                    t_sum = pr[min_index].at + pr[min_index].bt;
+                    pr.RemoveAt(min_index);
+                    j++;
                 }
                 else
                 {
-                    t_sum += bt[i - 1];
-                    if (i != size && t_sum < at[i])
-                        st[i] = at[i];
-                    else
-                        st[i] = t_sum;
+                    st[j] = t_sum;
+                    ps[j] = pr[i].pid;
+                    at[j] = pr[i].at;
+                    t_sum += pr[i].bt;
+                    pr.RemoveAt(i);
+                    j++;
                 }
             }
+            st[j] = t_sum;
         }
         public static float avgWaiting(int size, int[] at, int[] st)
         {
             float wt = 0;
-            for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
             {
-                wt += st[i] - at[i];
+                wt += st[j] - at[j];
             }
             wt /= (float)size;
             return wt;
@@ -65,9 +89,9 @@ namespace CPU_SCHEDULERS
         public static float avgTurnAround(int size, int[] at, int[] st)
         {
             float tat = 0;
-            for (int i = 0; i < size; i++)
+            for (int k = 0; k < size; k++)
             {
-                tat += st[i+1] - at[i];
+                tat += st[k+1] - at[k];
             }
             tat /= (float)size;
             return tat;
