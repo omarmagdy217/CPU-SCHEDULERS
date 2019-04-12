686237add17f99f62c2975552be5d0199cd56383
omarmagdy217 <36456356+omarmagdy217@users.noreply.github.com>
2019-04-12 20:11:31 +0200

    Priority DONE

--- Main Code/PRIORITY.cs
+++ Main Code/PRIORITY.cs
@@ -16,7 +16,7 @@ namespace CPU_SCHEDULERS
             public int priority; // Priority of this process 
         };
 
-        private static void sortList(IList<Process> pr)
+        /*private static void sortList(IList<Process> pr)
         {
             Process temp;
             for (int iter = 0; iter < (pr.Count - 1); iter++)
@@ -31,61 +31,9 @@ namespace CPU_SCHEDULERS
                     }
                 }
             }
-        }
+        }*/
 
-        public static void prioritySortNonPreemptive(int size, int[] at, int[] bt, int[] prio, int[] st, int[] ps)
-        {
-            int t_sum = at[0], min_at, min_index, i, j = 0;
-            IList<Process> pr = new List<Process>();
-            Process temp;
-            for (i = 0; i < size; i++)
-            {
-                temp.pid = ps[i];
-                temp.at = at[i];
-                temp.bt = bt[i];
-                temp.priority = prio[i];
-                pr.Add(temp);
-                if (at[i] < t_sum)
-                    t_sum = at[i];
-            }
-            sortList(pr);
-            while (pr.Count > 0)
-            {
-                min_at = pr[0].at;
-                min_index = 0;
-                for (i = 0; i < pr.Count; i++)
-                {
-                    if (pr[i].at <= t_sum)
-                        break;
-                    else if (pr[i].at < min_at)
-                    {
-                        min_at = pr[i].at;
-                        min_index = i;
-                    }
-                }
-                if (i == pr.Count)
-                {
-                    st[j] = pr[min_index].at;
-                    ps[j] = pr[min_index].pid;
-                    at[j] = pr[min_index].at;
-                    t_sum = pr[min_index].at + pr[min_index].bt;
-                    pr.RemoveAt(min_index);
-                    j++;
-                }
-                else
-                {
-                    st[j] = t_sum;
-                    ps[j] = pr[i].pid;
-                    at[j] = pr[i].at;
-                    t_sum += pr[i].bt;
-                    pr.RemoveAt(i);
-                    j++;
-                }
-            }
-            st[j] = t_sum;
-        }
-
-        public static void prioritySortPreemptive(int size, int[] at, int[] bt, int[] prio, int[] ps, IList<int> stp, IList<int> psp)
+        public static void prioritySort(bool prm, int size, int[] at, int[] bt, int[] prio, int[] ps, IList<int> stp, IList<int> psp)
         {
             int t_sum = at[0], min_at, min_index, prev_index, i, j = 0;
             IList<Process> pr = new List<Process>();
@@ -101,7 +49,13 @@ namespace CPU_SCHEDULERS
                 if (at[i] < t_sum)
                     t_sum = at[i];
             }
-            sortList(pr);
+            //sortList(pr);
+            pr = pr.OrderBy(x => x.priority).ToList();
+            if (t_sum > 0)
+            {
+                stp.Add(0);
+                psp.Add(0);
+            }
             while (pr.Count > 0)
             {
                 min_at = pr[0].at;
@@ -120,92 +74,107 @@ namespace CPU_SCHEDULERS
                 }
                 if (i == pr.Count)
                 {
+                    stp.Add(t_sum);
+                    psp.Add(0);
                     stp.Add(pr[min_index].at);
                     psp.Add(pr[min_index].pid);
-                    atp.Add(pr[min_index].at);
                     t_sum = pr[min_index].at + pr[min_index].bt;
-                    if (pr[prev_index].at < t_sum && min_index != prev_index)
+                    if (prm)
                     {
-                        temp = pr[min_index];
-                        temp.bt = t_sum - pr[prev_index].at;
-                        temp.at = pr[prev_index].at + pr[prev_index].bt;
-                        pr[min_index] = temp;
-                        t_sum = pr[prev_index].at;
+                        atp.Add(pr[min_index].at);
+                        if (pr[prev_index].at < t_sum && min_index != prev_index)
+                        {
+                            temp = pr[min_index];
+                            temp.bt = t_sum - pr[prev_index].at;
+                            temp.at = pr[prev_index].at + pr[prev_index].bt;
+                            pr[min_index] = temp;
+                            t_sum = pr[prev_index].at;
+                        }
+                        else
+                            pr.RemoveAt(min_index);
                     }
                     else
+                    {
+                        at[j] = pr[min_index].at;
                         pr.RemoveAt(min_index);
+                        j++;
+                    }
                 }
                 else
                 {
                     stp.Add(t_sum);
                     psp.Add(pr[i].pid);
-                    atp.Add(pr[i].at);
                     t_sum += pr[i].bt;
-                    if (pr[min_index].at < t_sum && i != min_index)
+                    if(prm)
                     {
-                        temp = pr[i];
-                        temp.bt = t_sum - pr[min_index].at;
-                        temp.at = pr[min_index].at + pr[min_index].bt;
-                        pr[i] = temp;
-                        t_sum = pr[min_index].at;
+                        atp.Add(pr[i].at);
+                        if (pr[min_index].at < t_sum && i != min_index)
+                        {
+                            temp = pr[i];
+                            temp.bt = t_sum - pr[min_index].at;
+                            temp.at = pr[min_index].at + pr[min_index].bt;
+                            pr[i] = temp;
+                            t_sum = pr[min_index].at;
+                        }
+                        else
+                            pr.RemoveAt(i);
                     }
                     else
+                    {
+                        at[j] = pr[i].at;
                         pr.RemoveAt(i);
+                        j++;
+                    }
                 }
             }
             stp.Add(t_sum);
         }
 
-        public static float avgWaitingNonPreemptive(int size, int[] at, int[] st)
-        {
-            float wt = 0;
-            for (int j = 0; j < size; j++)
-            {
-                wt += st[j] - at[j];
-            }
-            wt /= (float)size;
-            return wt;
-        }
-
-        public static float avgTurnAroundNonPreemptive(int size, int[] at, int[] st)
-        {
-            float tat = 0;
-            for (int k = 0; k < size; k++)
-            {
-                tat += st[k+1] - at[k];
-            }
-            tat /= (float)size;
-            return tat;
-        }
-
-        public static float avgWaitingPreemptive(int size, int[] at, int[] bt, IList<int> stp, IList<int> psp)
+        public static float avgWaiting(bool prm, int size, int[] at, int[] bt, IList<int> stp, IList<int> psp)
         {
             float wt_sum = 0;
             int[] wt = new int[size];
             for (int j = 0; j < psp.Count; j++)
             {
-                wt[psp[j] - 1] = stp[j + 1] - at[psp[j] - 1] - bt[psp[j] - 1];
+                if (psp[j] != 0)
+                {
+                    if (prm)
+                        wt[psp[j] - 1] = stp[j + 1] - at[psp[j] - 1] - bt[psp[j] - 1];
+                    else
+                        wt_sum += stp[j] - at[psp[j] - 1];
+                }
             }
-            for (int k = 0; k < size; k++)
+            if (prm)
             {
-                wt_sum += wt[k];
+                for (int k = 0; k < size; k++)
+                {
+                    wt_sum += wt[k];
+                }
             }
             wt_sum /= (float)size;
             return wt_sum;
         }
 
-        public static float avgTurnAroundPreemptive(int size, int[] at, IList<int> stp, IList<int> psp)
+        public static float avgTurnAround(bool prm, int size, int[] at, IList<int> stp, IList<int> psp)
         {
             float tat_sum = 0;
             int[] tat = new int[size];
             for (int j = 0; j < psp.Count; j++)
             {
-                tat[psp[j]-1] = stp[j+1] - at[psp[j]-1];
+                if (psp[j] != 0)
+                {
+                    if (prm)
+                        tat[psp[j] - 1] = stp[j + 1] - at[psp[j] - 1];
+                    else
+                        tat_sum += stp[j + 1] - at[psp[j] - 1];
+                }
             }
-            for (int k = 0; k < size; k++)
+            if (prm)
             {
-
-                tat_sum += tat[k];
+                for (int k = 0; k < size; k++)
+                {
+                    tat_sum += tat[k];
+                }
             }
             tat_sum /= (float)size;
             return tat_sum;
